a884903483e7cfd3e5987c3cc9a48d75ca1012fd
gpmatheus <63263215+gpmatheus@users.noreply.github.com>
2025-03-01 18:26:02 -0300

    fixed scrol controller bug then creating a content

--- lib/ui/note_details/widgets/note_details_view.dart
+++ lib/ui/note_details/widgets/note_details_view.dart
@@ -26,7 +26,7 @@ class _NoteScreenState extends State<NoteScreen> with SingleTickerProviderStateM
   final ScrollController _scrollController = ScrollController();
   late AnimationController _animationController;
   late Animation<double> _animation;
-  List<GlobalKey>? _keys;
+  final List<GlobalKey> _keys = [];
 
   @override
   void initState() {
@@ -42,19 +42,6 @@ class _NoteScreenState extends State<NoteScreen> with SingleTickerProviderStateM
         curve: Curves.easeInOut,
       )
     );
-
-    widget.viewModel.addListener(() {
-      if (widget.viewModel.note != null) {
-        if (_keys == null) {
-          _keys = [
-            for (int i = 0; i < widget.viewModel.note!.contents!.length; i++)
-              GlobalKey()
-          ];
-        } else if (_keys!.length != widget.viewModel.note!.contents!.length) {
-          _keys!.add(GlobalKey());
-        }
-      }
-    });
   }
 
   @override
@@ -72,6 +59,12 @@ class _NoteScreenState extends State<NoteScreen> with SingleTickerProviderStateM
         listenable: widget.viewModel,
         builder: (context, _) {
           final Note? note = widget.viewModel.note;
+          if (note != null && note.contents != null && _keys.length != note.contents!.length) {
+            _keys.clear();
+            _keys.addAll(
+              List.generate(note.contents!.length, (index) => GlobalKey())
+            );
+          }
           _displayContentDeletedResult(
             widget.viewModel.showContentDeletedResult,
             widget.viewModel.deletedSuccess,
@@ -92,7 +85,7 @@ class _NoteScreenState extends State<NoteScreen> with SingleTickerProviderStateM
                       children: List.generate(note.contents!.length, (index) {
                         final content = note.contents![index];
                         return ContentContainer(
-                          key: _keys![index],
+                          key: _keys[index],
                           actions: [
                             ActionsContent(
                               icon: const Icon(Icons.delete_rounded), 
@@ -200,7 +193,7 @@ class _NoteScreenState extends State<NoteScreen> with SingleTickerProviderStateM
   }
 
   void _animateEditedContent(int index) {
-    final RenderBox renderBox = _keys![index].currentContext!.findRenderObject() as RenderBox;
+    final RenderBox renderBox = _keys[index].currentContext!.findRenderObject() as RenderBox;
     final double itemHeight = renderBox.size.height;
     final double viewportHeight = MediaQuery.of(context).size.height;
     final double offset = _calculateOffset(index, itemHeight, viewportHeight);
@@ -209,9 +202,12 @@ class _NoteScreenState extends State<NoteScreen> with SingleTickerProviderStateM
   }
 
   void _animateCreatedContent() {
-    final double offset = 
-    (_scrollController.hasClients ? _scrollController.position.maxScrollExtent : 0.0) +
-    (MediaQuery.of(context).size.height / 4);
+    if (_keys.isEmpty) return;
+    final int index = _keys.length - 1;
+    final RenderBox renderBox = _keys[index].currentContext!.findRenderObject() as RenderBox;
+    final double itemHeight = renderBox.size.height;
+    final double viewportHeight = MediaQuery.of(context).size.height;
+    final double offset = _calculateOffset(index, itemHeight, viewportHeight);
 
     _centerWidget(offset);
   }
@@ -243,7 +239,7 @@ class _NoteScreenState extends State<NoteScreen> with SingleTickerProviderStateM
   double _calculateOffset(int index, double itemHeight, double viewportHeight) {
     double offset = 0;
     for (int i = 0; i < index; i++) {
-      final RenderBox renderBox = _keys![i].currentContext!.findRenderObject() as RenderBox;
+      final RenderBox renderBox = _keys[i].currentContext!.findRenderObject() as RenderBox;
       offset += renderBox.size.height;
     }
     offset = offset - (viewportHeight / 2) + itemHeight;
